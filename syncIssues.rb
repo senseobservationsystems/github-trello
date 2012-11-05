4c02fc508a1b5c9f3777e1a7c0767fb3da6feffc
Deploy <deploy@sense-os.nl>
2012-11-05 10:26:15 +0100

    Updated syncIssues to work with Steven's configuration

--- syncIssues.rb
+++ syncIssues.rb
@@ -28,45 +28,60 @@ github = Github.new #basic_auth: 'user:password'
 ARGV.each do|a|
 	user,repos = a.split("/")
 	repos_url = "https://github.com/#{a}"
-	list_id = config["issue_list"][repos]
-	unless list_id 
-		list_id = config["issue_list"]["default"]
-	end
-	unless list_id
-		puts "[ERROR] Issue from #{repos} but no list_id entry nor default entry found in config"
-		return
-	end
 	issues = github.issues.list_repo user, repos
 	#select open, new issues
 	open_issues = issues.select {|issue| issue['state'] == 'open' and db.execute("SELECT COUNT(1) FROM cards WHERE issue_id == #{issue['id']}")[0][0] == 0}
 	puts "Found #{open_issues.count} new open issues in #{a}.\n"
 
 	open_issues.each do |issue|
-		#create card
-		card_name = "#{issue["title"]}"
-		card_desc = "#{issue["body"]}\n\nGithub [issue #{issue["number"]}](#{issue["html_url"]}) in [#{a}](#{repos_url})."
-		card = {}
-		card[:name] = card_name
-		card[:desc] = card_desc
-		card[:idList] = list_id
-		puts "\tPosting card #{card.inspect}.\n"
-		response = http.add_card(card)
-		puts response.inspect
-		response = JSON.parse(response)
-		if (response and response.has_key?('id'))
-			card_id = response['id']
-			#add to database as well
-			puts "Putting (#{card_id},#{issue['id']}) into database.\n"
-			db.execute("insert into cards (card_id,issue_id) values ('#{card_id}','#{issue['id']}')")
-			#add label
-			label = config['repos_labels'][repos]
-			unless label
-				label = config['repos_labels']['default']
-			end
-			if (label)
-				http.add_label card_id, label
+      # find repository type (backend, api lib, etc)
+      repo_name = a
+      repo_url = repos_url
+      repo_type = config["repo_types"][repo_name]
+      unless repo_type
+        repo_type = config["repo_types"]["default"]
+      end
+
+      # see if the issue has been labeled as an enhancement in GitHub
+      labels = issue["labels"]
+      is_enhancement = false
+      labels.each do|label|
+        if label["name"] == "enhancement"
+          is_enhancement = true
+        end
+      end
+      
+      # get the proper Trello list ID from the config
+      if is_enhancement
+        list_id = config["inbox_lists"][repo_type]
+      else
+        list_id = config["bugs_lists"][repo_type]
+      end
+      unless list_id
+        puts "[ERROR] Issue from #{repo_name} but no list_id entry nor default entry found in config"
+        return
+      end
+
+      # parse issue to create a nice card name and description with a link to the issue
+      if is_enhancement
+        card_name = "#{issue["title"]}"
+      else
+        card_name = "Bug: #{issue["title"]}"
+      end
+      card_desc = "#{issue["body"]}\n\nGithub [issue #{issue["number"]}](#{issue["html_url"]}) in [#{repo_name}](#{repo_url})."
+      card = {}
+      card[:name] = card_name
+      card[:desc] = card_desc
+      card[:idList] = list_id
+      puts "Posting card #{card.inspect}."
+      response = http.add_card(card)
+			response = JSON.parse(response)
+			if (response and response.has_key? 'id')
+				card_id = response['id']
+				#add to database as well
+				puts "Putting (#{card_id},#{issue['id']}) into database.\n"
+				db.execute("insert into cards (card_id,issue_id) values ('#{card_id}','#{issue['id']}')")
 			end
 
-		end
 	end
 end
