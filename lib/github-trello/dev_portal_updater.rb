b317c7bd9f81594e0fd6e2cacb9d6a072039dbe1
Deploy <deploy@sense-os.nl>
2012-11-27 15:46:20 +0100

    add resque task to put output on log

--- lib/github-trello/dev_portal_updater.rb
+++ lib/github-trello/dev_portal_updater.rb
@@ -3,12 +3,24 @@ module GithubTrello
     @queue = :devportal
 
     def self.perform(devportal_path, devportal_pid)
-      puts "Updating devportal" 
+      puts "Updating devportal, path: #{devportal_path}, unicorn pid: #{devportal_pid}" 
       pid = `cat #{devportal_pid}`
       unless pid.empty?
-        cmd = "cd #{devportal_path};git reset --hard HEAD;git pull origin master;  LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 RACK_ENV=production rake build; kill -HUP #{pid}"
-        result = `#{cmd}`
-        puts result
+
+        puts "#{Time.now} update codebase from github"
+        cmd = "cd #{devportal_path};git reset --hard HEAD;git pull origin master;"
+        puts `#{cmd}`
+
+        puts "#{Time.now} Parse articles into database"
+        cmd = "cd #{devportal_path};LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 BUNDLE_GEMFILE='#{devportal_path}/Gemfile' RACK_ENV=production rake build --trace"
+
+        puts `#{cmd}`
+
+        puts "#{Time.now} restart unicorn_devportal service"
+        cmd = "kill -HUP #{pid}"
+        puts `#{cmd}`
+      else
+        puts "PID of devportal is empty"
       end
     end
   end
